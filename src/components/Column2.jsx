723778634797d41e28690ffbb9254fb64cdebe3b
belem <graceitamunoala@gmail.com>
2023-06-20 19:16:09 +0100

    styled colums2

--- src/components/Column2.jsx
+++ src/components/Column2.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import crashCourse from '../assets/crash-course.PNG'
+import framer from '../assets/framer-motion.PNG'
+import paystack from '../assets/paystack.PNG'
 
 const Column2 = () => {
   return (
@@ -8,17 +11,51 @@ const Column2 = () => {
           Watch history
         </div>
 
-        <div className='mb-3'>
+        <div className='mb-6'>
           Today
         </div>
 
-        <div className='flex flex-col'>
-          <div>
-            <img />
+        <div className='flex grid-cols-2'>
+          <div className=''>
+            <img src={framer} className='rounded-xl' />
           </div>
 
-          <div>
-            <p></p>
+          <div className='max-w-sm ml-4'>
+            <p className='text-lg'>Framer Motion (for React) #1 - <br/> Introduction</p>
+            <p className='text-gray-500 text-xs'>The Net Naija <span className='ml-6'>198k Views</span></p>
+            <p className='text-xs text-gray-500 mt-4'>Hey gang - welcome to your first Frame Motion (for React) tutorial. In 
+            this tutorial, I'll explain what Framer Motion is and what you'll need t...
+            </p>
+          </div>
+
+        </div>
+
+        <div className='flex grid-cols-2 mt-3'>
+          <div className=''>
+            <img src={crashCourse} className='rounded-xl' />
+          </div>
+
+          <div className='max-w-sm ml-4'>
+            <p className='text-lg'>React JS Crash Course</p>
+            <p className='text-gray-500 text-xs'>Traversy Media<span className='ml-6'>198k Views</span></p>
+            <p className='text-xs text-gray-500 mt-4'>Get started with React in this crash course. We will be building a task 
+            <br/>tracker app and look at components, props, state, hooks, working...
+            </p>
+          </div>
+
+        </div>
+
+        <div className='flex grid-cols-2 mt-3'>
+          <div className=''>
+            <img src={paystack} className='rounded-xl' />
+          </div>
+
+          <div className='max-w-sm ml-4'>
+            <p className='text-lg'>Framer Motion (for React) #1 - <br/> Introduction</p>
+            <p className='text-gray-500 text-xs'>The Net Naija <span className='ml-6'>198k Views</span></p>
+            <p className='text-xs text-gray-500 mt-4'>Hey gang - welcome to your first Frame Motion (for React) tutorial. In 
+            this tutorial, I'll explain what Framer Motion is and what you'll need t...
+            </p>
           </div>
 
         </div>
