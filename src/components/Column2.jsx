4f232d6e4a4fcd06700383bb682ac2ad6d63f232
belem <graceitamunoala@gmail.com>
2023-06-21 16:48:15 +0100

    added some icons

--- src/components/Column2.jsx
+++ src/components/Column2.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import crashCourse from '../assets/crash-course.PNG'
 import framer from '../assets/framer-motion.PNG'
 import paystack from '../assets/paystack.PNG'
+import {MdVerified} from 'react-icons/md'
 
 const Column2 = () => {
   return (
@@ -20,9 +21,12 @@ const Column2 = () => {
             <img src={framer} className='rounded-xl' />
           </div>
 
-          <div className='max-w-sm ml-4 text-justify'>
+          <div className='max-w-sm ml-4'>
             <p className='text-lg'>Framer Motion (for React) #1 - <br/> Introduction</p>
-            <p className='text-white text-opacity-50 text-xs'>The Net Naija <span className='ml-6'>198k Views</span></p>
+            <p className='text-white text-opacity-50 text-xs flex'>The Net Naija
+            <span><MdVerified size={15} className='text-gray-500 mt-0 ml-1'/></span>
+            <span className='ml-4'>198k Views</span>
+            </p>
             <p className='text-xs text-white text-opacity-50 mt-1'>Hey gang - welcome to your first Frame Motion (for React) tutorial. In 
             this tutorial, I'll explain what Framer Motion is and what you'll need t...
             </p>
@@ -35,9 +39,12 @@ const Column2 = () => {
             <img src={crashCourse} className='rounded-xl' />
           </div>
 
-          <div className='max-w-sm ml-4 text-justify'>
+          <div className='max-w-sm ml-4'>
             <p className='text-lg'>React JS Crash Course</p>
-            <p className='text-white text-opacity-50 text-xs'>Traversy Media<span className='ml-6'>3.1M Views</span></p>
+            <p className='text-white text-opacity-50 text-xs flex'>Traversy Media
+            <span><MdVerified size={15} className='text-gray-500 mt-0 ml-1'/></span>
+            <span className='ml-4'>3.1M Views</span>
+            </p>
             <p className='text-xs text-white text-opacity-50 mt-2'>Get started with React in this crash course. We will be building a task 
             tracker app and look at components, props, state, hooks, working...
             </p>
@@ -50,9 +57,12 @@ const Column2 = () => {
             <img src={paystack} className='rounded-xl' />
           </div>
 
-          <div className='max-w-sm ml-4 text-justify'>
+          <div className='max-w-sm ml-4'>
             <p className='text-lg'>Paystack Payment Verification<br/> integration in Python Django</p>
-            <p className='text-white text-opacity-50 text-xs'>BiggiDroid<span className='ml-6'>3.4k Views</span></p>
+            <p className='text-white text-opacity-50 text-xs flex'>BiggiDroid
+            <span><MdVerified size={15} className='text-gray-500 mt-0 ml-1'/></span>
+            <span className='ml-4'>3.4k Views</span>
+            </p>
             <p className='text-xs text-white text-opacity-50 mt-1'>Getting Paystack payment verification integration is one of the best
             ideas to accept payments on an e-commerce website. Sourcecode:...
             </p>
