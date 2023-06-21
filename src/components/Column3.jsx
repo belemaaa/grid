7caa0b2f0796ca4b24687cb820f62a13a7ecbed6
belem <graceitamunoala@gmail.com>
2023-06-21 19:19:13 +0100

    committed column 3

--- src/components/Column3.jsx
+++ src/components/Column3.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import {AiOutlineSearch} from 'react-icons/ai'
+import {BsCircle} from 'react-icons/bs'
+import {BsRecordCircle} from 'react-icons/bs'
+import {TfiTrash} from 'react-icons/tfi'
+import {AiOutlinePause} from 'react-icons/ai'
+import {LuSettings} from 'react-icons/lu'
 
 const Column3 = () => {
   return (
@@ -13,14 +18,28 @@ const Column3 = () => {
 
           <div className='flex flex-col text-sm'>
             <p className='mt-8 border-b border-b-gray-500 pb-4'>History type</p>
-            <p className='mt-4 border-b border-b-gray-500 pb-4 flex flex-row'>Watch history</p>
-            <p className='mt-4 border-b border-b-gray-500 pb-4 mb-6 flex flex-row'>Community</p>
+            <p className='mt-4 border-b border-b-gray-500 pb-4 flex flex-row'>Watch history
+            <BsRecordCircle size={20} className='ml-44 text-blue-300'/> </p>
+            <p className='mt-4 border-b border-b-gray-500 pb-4 mb-6 flex flex-row'>Community
+            <BsCircle size={18} className='ml-48 text-white'/> </p>
           </div>
 
-          <div className='flex flex-col'>
-            <p className='mb-4 flex flex-row'>Clear all watch history</p>
-            <p className='mb-4 flex flex-row'>Pause watch history</p>
-            <p className='mb-4 flex flex-row'>Manage all history</p>
+          <div className='flex flex-col text-sm'>
+            <p className='mb-4 flex flex-row rounded-full bg-white bg-opacity-20 w-fit px-3 py-2'>
+              <TfiTrash size={20} className='text-white mr-3'/>
+              Clear all watch history</p>
+            <p className='mb-4 flex flex-row rounded-full bg-white bg-opacity-20 w-fit px-3 py-2'>
+              <AiOutlinePause size={20} className='text-white mr-3'/>
+              Pause watch history</p>
+            <p className='mb-4 flex flex-row rounded-full bg-white bg-opacity-20 w-fit px-3 py-2'>
+              <LuSettings size={20} className='text-white mr-3'/>
+              Manage all history</p>
+          </div>
+
+          <div className='flex flex-col text-xs ml-12 text-white text-opacity-50'>
+            <p className='mb-4'>Watch and search history</p>
+            <p className='mb-4'>Comments</p>
+            <p className=''>Live chat</p>
           </div>
         </div>
     </div>
